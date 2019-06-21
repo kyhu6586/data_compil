652b98187e2148f1067df28a41791638c9068143
Kylie Hunter <kyhu6586@colorado.edu>
2019-06-21 13:46:27 -0400

    Reads in preceding neighbor to file

--- anglesOnly.py
+++ anglesOnly.py
@@ -6,19 +6,28 @@ traj = md.load('1igd_processed.pdb')
 print(traj)
 index=0
 indices=[]
-nums=[]
+names=[]
+prevs=[]
 topology=traj.topology
+resid=[]
+for res in topology.residues:
+    resid.append(res)
 #Loop through this topology, grabbing the first four atoms from every residue
 #(Except glycine and prolyne)
-for res in topology.residues:
+for res in range(len(resid)):
     mini=[] #We want every residue's atoms to be in its own list
-    for num in range(len(list(res.atoms))):
-        if num<4 and res.name != 'GLY' and res.name != 'PRO' and res.name != 'HOH':
-           # print(list(res.atoms)[num])
+    for num in range(len(list(resid[res].atoms))):
+        if num<4 and resid[res].name != 'GLY' and resid[res].name != 'PRO' and resid[res].name != 'HOH':
+            if res>0 and num==0:
+                prevs.append(resid[res-1].name)
+            elif res==0 and num==0:
+                prevs.append("")
+                print("empty string appended")
+            print(list(resid[res].atoms)[num])
             mini.append(index) #append these indices to an array
             index+=1
             if num==0:
-                nums.append(res) # Build an array with all the res names
+                names.append(resid[res].name) # Build an array with all the res names
         else: #Throw out the rest of the indices (after first 4) til next residue
             index+=1
     if len(mini) > 0:#As long as mini has elements in it, append it to 2D indices
@@ -28,20 +37,18 @@ print("----------------")
 #Make our angles :)
 angles=[]
 for ind in indices:
-    #print(ind)
     if len(ind)==4:
         newlist=np.array([ind[1], ind[0], ind[2], ind[3]])
         newlist=np.reshape(newlist,(1,4))
         angles.append(md.compute_dihedrals(traj, newlist)[0][0])
 
-f=open('1igd_angles.txt','w+')
-for val in range(len(nums)):
-    f.write((str)(nums[val]))
-    f.write(" ")
+f=open('1IGD_angles.txt','w+')
+for val in range(len(names)):
+    f.write(names[val]+" ")
+    f.write(prevs[val]+" ")
     f.write((str)(angles[val]))
-    f.write(" ")
     f.write("\n")
 f.close()
     #CHANGE THIS TOO
-print("File written to 1igd_angles.txt")
+print("File written to 1IGD_angles.txt")
 
--- md.py
+++ md.py
@@ -2,23 +2,55 @@ from __future__ import print_function
 import mdtraj as md
 import numpy as np
 #Change this to load in desired file-- must be processed through gromacs
-traj = md.load('1igd_processed.pdb')
+loadfile = "1FZT_processed.pdb"
+prot=loadfile[:4]
+traj = md.load(loadfile)
 print(traj)
+
+class Data:
+    res=""
+    neighbor=""
+    atoms=[]
+    couple=0.0
+    def __init__(self,r,n,a,c):
+        self.res=r
+        self.neighbor=n
+        self.atoms=a
+        self.couple=c
+    def __init__(self,r,n,a):
+        self.res=r
+        self.neighbor=n
+        self.atoms=a
+    def __init__(self,r,a):
+        self.res=r
+        self.atoms=a
+    def couple(self,c):
+        self.couple=c
+
 index=0
 indices=[]
 names=[]
+prevs=[]
 topology=traj.topology
+resid=[]
+for res in topology.residues:
+    resid.append(res)
 #Loop through this topology, grabbing the first four atoms from every residue
 #(Except glycine and prolyne)
-for res in topology.residues:
+for res in range(len(resid)):
     mini=[] #We want every residue's atoms to be in its own list
-    for num in range(len(list(res.atoms))):
-        if num<4 and res.name != 'GLY' and res.name != 'PRO' and res.name != 'HOH':
-          #  print(list(res.atoms)[num])
+    for num in range(len(list(resid[res].atoms))):
+        if num<4 and resid[res].name != 'GLY' and resid[res].name != 'PRO' and resid[res].name != 'HOH':
+            if res>0 and num==0:
+                prevs.append(resid[res-1].name)
+            elif res==0 and num==0:
+                prevs.append("")
+                print("empty string appended")
+            print(list(resid[res].atoms)[num])
             mini.append(index) #append these indices to an array
             index+=1
             if num==0:
-                names.append(res.name) # Build an array with all the res names
+                names.append(resid[res].name) # Build an array with all the res names
         else: #Throw out the rest of the indices (after first 4) til next residue
             index+=1
     if len(mini) > 0:#As long as mini has elements in it, append it to 2D indices
@@ -28,7 +60,6 @@ print("----------------")
 #Make our angles :)
 angles=[]
 for ind in indices:
-    print(ind)
     if len(ind)==4:
         newlist=np.array([ind[1], ind[0], ind[2], ind[3]])
         newlist=np.reshape(newlist,(1,4))
@@ -40,13 +71,15 @@ for name in names:
 names2=[]
 couples=[]
 #Put a different file name here
-r=open("couples1igd.txt","r")
+r=open("couples"+prot.lower()+".txt","r")
 for line in r:
-    split=line.split()
-    if(split[8]=="HA"):
-        names2.append(split[7])
-        couples.append(split[22])
+    if line!="":
+        split=line.split() 
+        if len(split)>0:
+            names2.append(split[7])
+            couples.append(split[22])
 #Code to ensure the indices of the angles and couples line up using name information
+print(names2)
 print(len(names2))
 print(len(couples))
 print(len(names))
@@ -63,11 +96,14 @@ while num<len(names) and num<len(names2):
         print("Deleting from names1 at num %d"%num)
         names.pop(num)
         angles.pop(num)
+        prevs.pop(num)
         num-=1
     num+=1
 
 if len(names)==len(names2)+1:
     names.pop()
+    angles.pop()
+    prevs.pop()
 
 if len(names)!=len(names2): 
     print(len(names))
@@ -76,9 +112,7 @@ if len(names)!=len(names2):
 
 #Last Step!!
 #Now let's write this data to a file!!
-
-    #CHANGE THIS CHANGE THIS CHANGE THIS
-f=open('1igd_angles.txt','w+')
+f=open(prot+'_angles.txt','w+')
 val=0
 if(len(names)>len(names2)):
     val=len(names2)
@@ -86,10 +120,10 @@ else:
     val=len(names)
 for val in range(val):
     f.write(names[val]+" ")
+    f.write(prevs[val]+" ")
     f.write((str)(angles[val]))
     f.write(" ")
     f.write(couples[val])
     f.write("\n")
 f.close()
-    #CHANGE THIS TOO
-print("File written to 1igd_angles.txt")
+print("File written to "+prot+"_angles.txt")
