f81d02627b131787209b38edf464035dbd339a4c
Kylie Hunter <kyhu6586@colorado.edu>
2019-07-19 15:22:00 -0400

    hTest functions

--- hTest.py
+++ hTest.py
@@ -7,9 +7,9 @@ C0=0.0
 A1=0.0
 B1=0.0
 C1=0.0
-overA=0.0
-overB=0.0
-overC=0.0
+A=0.0
+B=0.0
+C=0.0
 class Data1:
     resName=""
     neiName=""
@@ -35,57 +35,91 @@ def file_len(fname):
 
     return count+1
 
-f= open("C_Final.txt","r")
+f= open("1MVG_angles.txt","r")
 count=0
 h=[]
 noH=[]
 data=[]
-length=file_len("C_Final.txt")
-for line in r:
+length=file_len("1MVG_angles.txt")
+for line in f:
     split=line.split()
     if len(split)>1:
-        data.append(Data1(split[0],split[1],float(split[2]),float(split[3]),split[5]))
-        if data[count].h==0:
+        data.append(Data1(split[0],split[1],float(split[2]),float(split[3]),int(split[5])))
+        print(data[count].h)
+        if data[count].h%2==0:
             noH.append(data[count])
         else:
-            H.append(data[count])
+            h.append(data[count])
         count+=1
 index=0
 
 #random.shuffle(data)
 in_train=np.zeros([length,1],dtype=np.float32)
-out_train=np.zeros([length,1],dtype=np.float32)
+out0=np.zeros([length,1],dtype=np.float32)
+out1=np.zeros([length,1],dtype=np.float32)
+out=np.zeros([length,1],dtype=np.float32)
+karplus0=np.zeros([length,3],dtype=np.float32)
+karplus1=np.zeros([length,3],dtype=np.float32)
 karplus=np.zeros([length,3],dtype=np.float32)
-while index<len(data):
-    in_train[index]=data[index].angle
-    out_train[index]=data[index].constant
-    karplus[index][0]=1.0
-    karplus[index][1]=math.cos(float(in_train[index]))
-    karplus[index][2]=math.cos(2*float(in_train[index]))
-    index+=1
-wexact=np.linalg.pinv(karplus).dot(out_train)
-overA=wexact[0]
-overB=wexact[1]
-overC=wexact[2]
+for x in range(len(h)):
+    out1[x]=h[x].constant
+    karplus1[x][0]=1.0
+    karplus1[x][1]=math.cos(float(h[x].angle))
+    karplus1[x][2]=math.cos(2*float(h[x].angle))
+    print(karplus1[x])
+print("--------------------------------")
+for x in range(len(noH)):
+    out0[x]=noH[x].constant
+    karplus0[x][0]=1.0
+    karplus0[x][1]=math.cos(float(noH[x].angle))
+    karplus0[x][2]=math.cos(2*float(noH[x].angle))
+    print(karplus0[x])
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+for x in range(len(data)):
+    out[x]=data[x].constant
+    karplus[x][0]=1.0
+    karplus[x][1]=math.cos(float(data[x].angle))
+    karplus[x][2]=math.cos(2*float(data[x].angle))
+    print(karplus[x])
+print("************************************")
+wexact0=np.linalg.pinv(karplus0).dot(out0)
+wexact1=np.linalg.pinv(karplus1).dot(out1)
+wexact=np.linalg.pinv(karplus).dot(out)
+A0=wexact0[0]
+B0=wexact0[1]
+C0=wexact0[2]
+A1=wexact1[0]
+B1=wexact1[1]
+C1=wexact1[2]
+A=wexact[0]
+B=wexact[1]
+C=wexact[2]
+print("ABC "+str(A)+str(B)+str(C))
+print(len(data))
+print("A1B1C1 "+str(A1)+str(B1)+str(C1))
+print(len(h))
+print("A0B0C0 "+str(A0)+str(B0)+str(C0))
+print(len(noH))
+
+
+
 index=0
 testRes=0.0
 diffGen=0.0
 diffRes=0.0
-print("ABC")
-print(overA)
-print(overB)
-print(overC)
 print("~~~~~~~~~~~")
-
+#Run test cases to find deviation
 for index in range(len(data)):
     t=data[index]
-    here=dictionary[t.resName]
-    genDiff=t.constant-(overA+(overB*math.cos(t.angle))+(overC*math.cos(2*t.angle)))
-    resDiff=t.constant-(din[1][here]+(din[2][here]*math.cos(t.angle))+(din[3][here]*math.cos(2*t.angle)))
+    genDiff=t.constant-(A+(B*math.cos(t.angle))+(C*math.cos(2*t.angle)))
+    if t.h==0:
+        resDiff=t.constant-(A0+(B0*math.cos(t.angle))+(C0*math.cos(2*t.angle)))
+    else:
+        resDiff=t.constant-(A1+(B1*math.cos(t.angle))+(C1*math.cos(2*t.angle)))
     diffGen+=abs(genDiff[0])
-    diffRes+=abs(resDiff)
-    print(abs(genDiff[0]),abs(resDiff))
-    if resDiff<genDiff[0]:
+    diffRes+=abs(resDiff[0])
+    print(abs(genDiff[0]),abs(resDiff[0]))
+    if resDiff[0]<genDiff[0]:
         testRes+=1
 
 testRes/=len(data)
